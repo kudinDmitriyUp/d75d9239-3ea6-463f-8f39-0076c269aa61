6f2988cde3c4df5a1ec4b46cb4677b42418483ad
kudinDmitriyUp <81634209+kudinDmitriyUp@users.noreply.github.com>
2025-11-11 13:33:55 +0200

    Update project files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -105,7 +105,7 @@ export default function Home() {
           ]}
           buttons={[
             {
-              text: "Read More",
+              text: "Start Reading Free",
               href: "blog"
             }
           ]}
